164bf728cd1477bd54d8c2a17f4e4a403c786a1d
samugauna7job <sgauna@wecom.global>
2024-05-08 13:45:30 -0300

    is valid super user

--- index.js
+++ index.js
@@ -15,23 +15,39 @@ const io = new SocketIoServer(server, {
     transports: ['websocket']
 })
 app.use(cors())
-io.on('connection', (socket) => {
-    console.log(`a user connected ${socket.id}`)
-    const { superUserId } = socket.handshake.query; // Asegúrate de enviar este dato desde el cliente
-    if( isValidSuperUser(superUserId)){
-        socket.join(superUserId);
-        console.log(`Socket ${socket.id} joined room ${superUserId}`);
+io.on('connection', async (socket) => {  // Convertimos el manejador en una función asíncrona
+    console.log(`a user connected ${socket.id}`);
+    const { superUserId } = socket.handshake.query;
+
+    // Ahora podemos usar await para obtener el resultado de isValidSuperUser
+    try {
+        const isValid = await isValidSuperUser(superUserId);
+        console.log(isValid);  // Ahora debería mostrar true o false
+
+        if (isValid) {
+            socket.join(superUserId);
+            console.log(`Socket ${socket.id} joined room ${superUserId}`);
+
+            socket.on('register', (data) => {
+                console.log('message received from client to server:', data);
+                io.to(superUserId).emit('update', { message: 'desde el server, datos de update', data });
+            });
+        } else {
+            console.log(`SuperUserId no válido: ${superUserId}`);
+            socket.disconnect();
+        }
+    } catch (error) {
+        console.log(`Error al validar el superUserId: ${superUserId}`, error);
+        socket.disconnect();
     }
-    socket.on('register', (data) => {
-        console.log('message received from client to server:', data);
-        io.to(superUserId).emit('update', { message: 'desde el server, datos de update', data });
-    });
-    
+
     socket.on('disconnect', (reason) => {
-        socket.leave(superUserId)
         console.log(`Cliente ${socket.id} desconectado: ${reason}`);
+        if (superUserId) {
+            socket.leave(superUserId);
+        }
     });
-})
+});
 
 
 const PORT = process.env.PORT || 3000
